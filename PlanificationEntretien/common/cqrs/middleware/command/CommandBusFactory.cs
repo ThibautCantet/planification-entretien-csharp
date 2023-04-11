d12c40826c00849e09de1cce6f61b10858c85610
Thibaut Cantet <thibaut.cantet-extern@renault.fr>
2023-04-11 21:08:56 +0200

    wip

--- PlanificationEntretien/common/cqrs/middleware/command/CommandBusFactory.cs
+++ PlanificationEntretien/common/cqrs/middleware/command/CommandBusFactory.cs
@@ -1,16 +1,34 @@
 using System.Collections.Generic;
 using com.soat.planification_entretien.common.cqrs.command;
+using PlanificationEntretien.application_service;
+using PlanificationEntretien.candidat.application_service;
+using PlanificationEntretien.candidat.domain;
+using PlanificationEntretien.candidat.domain_service;
 using PlanificationEntretien.common.cqrs.middleware.evt;
+using PlanificationEntretien.entretien.application_service;
+using PlanificationEntretien.entretien.domain;
+using PlanificationEntretien.recruteur.application_service;
+using PlanificationEntretien.recruteur.domain;
 
 namespace PlanificationEntretien.common.cqrs.middleware.command;
 
 public class CommandBusFactory
 {
+    private readonly IEntretienRepository entretienRepository;
+    private readonly IEmailService emailService;
+    private readonly MessageBus messsageBus;
+    private readonly ICandidatRepository candidatRepository;
+    private readonly CandidatFactory candidatFactory;
+    private readonly IRecruteurRepository recruteurRepository;
 
     protected List<ICommandHandler> GetCommandHandlers()
     {
         return new List<ICommandHandler>
         {
+            new PlanifierEntretienCommandHandler(entretienRepository, emailService, messsageBus),
+            new CreerCandidatCommandHandler(candidatRepository, candidatFactory),
+            new ValiderEntretienCommandHandler(entretienRepository),
+            new CreerRecruteurCommandHandler(recruteurRepository, messsageBus)
         };
     }
 
