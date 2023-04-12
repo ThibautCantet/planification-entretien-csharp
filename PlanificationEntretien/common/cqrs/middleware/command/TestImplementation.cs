e4d7cd0b87b35b1e25931d8a46d2566b49957de3
Thibaut Cantet <thibaut.cantet-extern@renault.fr>
2023-04-12 15:57:25 +0200

    test to revert

---
+++ PlanificationEntretien/common/cqrs/middleware/command/TestImplementation.cs
@@ -0,0 +1,32 @@
+namespace PlanificationEntretien.common.cqrs.middleware.command;
+
+public class TestImplementation
+{
+    public ICommandHandler<Command> Get()
+    {
+        return new MyCommandHandler();
+    }
+
+    public interface ICommandHandler<C> where C : Command
+    {
+        int Handle(C command);
+    }
+
+    public interface Command // Updated to public
+    {
+
+    }
+
+    public class MyCommand : Command
+    {
+
+    }
+
+    public class MyCommandHandler : ICommandHandler<MyCommand> // Updated to use ICommandHandler
+    {
+        public int Handle(MyCommand command) // Updated to public
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
